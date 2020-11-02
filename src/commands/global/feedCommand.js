7be7a7b44a0ae0ce91b62487b8e5e6ba518d44c2
Poly <alexhexan22@gmail.com>
2020-11-02 19:11:05 +0100

    Basic feed generation working

--- src/commands/global/feedCommand.js
+++ src/commands/global/feedCommand.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default;
 
 
 class Feed {
@@ -19,9 +20,29 @@ class Feed {
             // Add identifiers with content to prevent colisions
             this.feeds[feedId] = {
                 interval: setInterval(what => {
-                        channel.send('A meme for your pleasures on: ' + what);
-                        channel.send('Feed id is: ' + feedId);
-                    }, 1E3, what, feedId),
+                        let feed = this.feeds[feedId];
+
+                        if (feed.fetchedContent.length > 0) {
+                            let next = feed.fetchedContent.pop();
+
+                            channel.send(next.title);
+                            channel.send(next.url);
+
+                            feed.seenContent.push(next);
+                        }
+
+                        if (feed.fetchedContent.length === 0) {
+                            console.log('Fetching more for feed', feedId);
+
+                            axios.get(`https://www.reddit.com/r/${what}/top.json?sort=top&t=day&limit=100`)
+                                .then(response => response.data.data.children)
+                                .then(data => {
+                                    feed.fetchedContent = data.map(post => post.data);
+                                })
+                                .catch(err => console.log(err));      
+                        }
+
+                    }, 1E4, what, feedId),
                 seenContent: [],
                 fetchedContent: []
             }
