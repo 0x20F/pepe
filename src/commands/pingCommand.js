299882499705db1c56d8e7d84349f744b4f6a18a
Poly <alexhexan22@gmail.com>
2020-11-02 17:19:21 +0100

    Cleaner

--- src/commands/pingCommand.js
+++ src/commands/pingCommand.js
@@ -1,10 +1,5 @@
-const { Command } = require('../command');
-
-
-class Ping extends Command {
+class Ping {
     boot = client => {
-        console.log('Boot running');
-
         client.on('message', message => {
             if (message.content === 'ping') {
                 message.reply('Pong!');
