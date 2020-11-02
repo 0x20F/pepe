d2182b10f94452b8b94eddbf7deb17081f09ed03
Poly <alexhexan22@gmail.com>
2020-11-02 17:11:50 +0100

    Architecture babyyyyyy!!!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
 const config = require('../config');
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
+
+const allCommands = (dir, _files) => {
+    files = fs.readdirSync(dir);
+  
+    _files = _files || [];
+  
+    files.forEach(file => {
+        if (fs.statSync(dir + "/" + file).isDirectory()) {
+            _files = getAllFiles(dir + "/" + file, _files);
+        } else {
+            _files.push(path.join(dir, "/", file));
+        }
+    });
+  
+    return _files;
+}
+
+/**
+ * Init all commands
+ * 
+ * 💕❤😊
+ */
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    let commands = [
-        require('./commands/pingCommand')
-    ];
+    let commands = allCommands(path.join(__dirname, 'commands'), []);
 
-    commands[0].boot(client);
+    commands.forEach(command => {
+        command = require(command);
+        command.boot(client);
+    });
 });
 
 client.login(config.token);
