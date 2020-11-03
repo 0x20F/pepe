21a264737f963257d9083a195013508bc5f09718
0x20F <alexhexan22@gmail.com>
2020-11-03 07:00:53 +0100

    Add more information on what's actually happening

--- src/commands/global/feed.js
+++ src/commands/global/feed.js
@@ -5,7 +5,7 @@ class Feed {
     feeds = {};
 
     boot = async client => {
-        client.on('message', message => {
+        client.on('message', async message => {
             let content = message.content;
 
             if (!content.startsWith('$feed')) {
@@ -13,42 +13,59 @@ class Feed {
             }
 
             // What subreddit to feed, and to what channel
-            let what = content.substring(content.indexOf(' ') + 1);
+            let subreddit = content.substring(content.indexOf(' ') + 1);
             let channel = message.channel;
             let feedId = channel.toString() + what;
 
+
             // Add identifiers with content to prevent colisions
             this.feeds[feedId] = {
-                interval: setInterval(async what => {
-                    let feed = this.feeds[feedId];
-
-                    if (feed.fetchedContent.length > 0) {
-                        let next = feed.fetchedContent.pop();
-
-                        channel.send(next.title);
-                        channel.send(next.url);
-
-                        feed.seenContent.push(next);
-                    }
-
-                    if (feed.fetchedContent.length === 0) {
-                        console.log('Fetching more for feed', feedId);
-                        await this.request(what, results => feed.fetchedContent = this.shuffle(results));
-                    }
-
-                }, 1E3 * 60 * 30, what, feedId),
+                id: feedId,
+                interval: setInterval(this.next(subreddit, feedId), 1E3 * 60 * 30, subreddit, feedId),
                 seenContent: [],
                 fetchedContent: []
             }
+
+
+            channel.send(`
+                Initialized feed for subreddit: ${subreddit}\n
+                Images/Posts will come in every 30 minutes.\n
+                Fetching top 100 posts of the week...
+            `);
+
+            // Initial fetch, and run it once
+            await this.request(subreddit, results => {
+                feed.fetchedContent = this.shuffle(results);
+                this.next(subreddit, feed.id);
+            });
         });
     }
 
 
     request = async (subreddit, callback) => {
-        axios.get(`https://www.reddit.com/r/${subreddit}/top.json?sort=top&t=day&limit=100`)
+        axios.get(`https://www.reddit.com/r/${subreddit}/top.json?sort=top&t=week&limit=100`)
             .then(response => response.data.data.children)
             .then(data => callback(data.map(post => post.data)))
             .catch(err => console.log(err));      
+    }
+
+
+    next = async (subreddit, feedId) => {
+        let feed = this.feeds[feedId];
+
+        if (feed.fetchedContent.length > 0) {
+            let next = feed.fetchedContent.pop();
+
+            channel.send(next.title);
+            channel.send(next.url);
+
+            feed.seenContent.push(next);
+        }
+
+        if (feed.fetchedContent.length === 0) {
+            console.log('Fetching more for feed', feedId);
+            await this.request(subreddit, results => feed.fetchedContent = this.shuffle(results));
+        }
     }
 
 
