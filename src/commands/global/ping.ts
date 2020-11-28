44b54b7e6e1c18c5447c8b3ba979bb3a4dfc388f
Poly <alexhexan22@gmail.com>
2020-11-28 14:42:59 +0100

    Move ping command to typescript

--- src/commands/global/ping.ts
+++ src/commands/global/ping.ts
@@ -1,1 +1,32 @@
 import Command from '@foundation/command';
+
+import { Client, Message } from 'discord.js';
+
+
+class Ping extends Command {
+    boot = async (c: Client): Promise<any> => {
+        c.on('message', message => {
+            if (!this.isPingPongMessage(message)) {
+                return;
+            }
+
+            let text = message.content;
+
+            let pongs = text.split(/[Pp]ing/gm);
+            let pings = pongs.map(p => p.split(/[Pp]ong/gm).join('ping'));
+            message.reply(pings.join('pong'));
+        });
+    }
+
+    isPingPongMessage = (message: Message): boolean => {
+        let textMessage = message.content.toLowerCase();
+        return (textMessage.includes('ping') || textMessage.includes('pong')) && !this.isBot(message);
+    }
+
+    isBot = (message: Message): boolean => {
+        return message.author.bot;
+    }
+}
+
+
+export default Ping;
