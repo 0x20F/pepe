f30d2ffcb8bd9bfce693f09818204a785bd059f4
Poly <alexhexan22@gmail.com>
2020-11-28 14:28:08 +0100

    Modules

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+import 'module-alias/register';
+
+
 import config from '../config.json';
 import { Client } from 'discord.js';
 
 // Ugly way of importing EVERYTHING
 import Feed from './commands/global/feed';
-import Ping from './commands/global/ping';
+//import Ping from './commands/global/ping';
 import Subreddits from './commands/global/subreddits';
 
 const client = new Client();
 
 const commands = [
     new Feed(),
-    new Ping(),
+    //new Ping(),
     new Subreddits()
 ];
 
